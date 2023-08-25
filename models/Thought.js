29d27df5c0083c4991c8ea531e794ed3d4f9fc83
Valeriereds <valeriereds@gmail.com>
2023-08-24 23:05:06 -0500

    Added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,26 @@
 // import moment.js for date format
 // reaction schema can go on this page
 // createdAt for both thought and reaction schema needs date formatting
+const moment = require ('moment')
+const {Schema, model} = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format('MMM, Do, YYYY [at] HH:MM a'),
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ reactionSchema ],
+  },
+);
