37c478c3f2096dafc0bb62c781c3c5771245e31a
Valeriereds <valeriereds@gmail.com>
2023-08-26 19:58:49 -0500

    finished thought/react schema, added user/thought controllers

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,29 @@
 // reaction schema can go on this page
 // createdAt for both thought and reaction schema needs date formatting
 const moment = require ('moment')
-const {Schema, model} = require('mongoose');
+const {Schema, model, Types} = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format('MMM, Do, YYYY [at] HH:MM a'),
+    }
+});
 
 const thoughtSchema = new Schema(
   {
@@ -23,4 +45,20 @@ const thoughtSchema = new Schema(
   },
   reactions: [ reactionSchema ],
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return `${this.reactions.length}`
+  });
+
+  const Thought = model('thought', thoughtSchema);
+
+  module.exports = Thought;
